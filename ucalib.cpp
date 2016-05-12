98602a165c3b733da6dda196456b6564d43d943f
Hendrik Siedelmann <hendrik.siedelmann@googlemail.com>
2016-05-12 13:23:21 +0200

    hotfix for marker detection

--- ucalib.cpp
+++ ucalib.cpp
@@ -728,6 +728,12 @@ static void _zline_problem_add_pinhole_lines(ceres::Problem &problem, cv::Point2
     if (isnan(p.x) || isnan(p.y))
       continue;
     
+    if (ray["x"] == 10 && ray["y"] == 10) {
+      std::cout << ray << std::endl;
+      std::cout << p << std::endl;
+      printf("%p - %f\n", &proxy(0, 10, 10, 0, 0, ray["views"]), proxy(0, 10, 10, 0, 0, ray["views"]));
+    }
+    
     double w;
     
     /*if (falloff)
@@ -1090,7 +1096,7 @@ int filter_pinhole(const Mat_<float>& proxy, Mat_<double> &lines, cv::Point2i im
  */
 //proxy.names({"point","x","y","channels","cams","views"});
 //lines.names({"line","x","y","channels","cams"})
-double fit_cams_lines_multi(const Mat_<float>& proxy, cv::Point2i img_size, Mat_<double> &lines, Mat_<double> &extrinsics, Mat_<double> &extrinsics_rel)
+double fit_cams_lines_multi(const Mat_<float>& proxy, cv::Point2i img_size, Mat_<double> &lines, Mat_<double> &extrinsics, Mat_<double> &extrinsics_rel, Mat_<double> &proj)
 {
   ceres::Solver::Options options;
   options.max_num_iterations = 5000;
@@ -1115,7 +1121,8 @@ double fit_cams_lines_multi(const Mat_<float>& proxy, cv::Point2i img_size, Mat_
   
   Mat_<double> r({proxy.r("channels","cams")});
   Mat_<double> m({2, proxy.r("channels","cams")});
-  Mat_<double> proj({2,proxy.r("channels","cams")});
+  
+  proj.create({2,proxy.r("channels","cams")});
   
   for(auto pos : Idx_It_Dims(r, 0, -1))
     r(pos) = 0;
@@ -1181,11 +1188,13 @@ double fit_cams_lines_multi(const Mat_<float>& proxy, cv::Point2i img_size, Mat_
   }*/
   
   
-  //options.max_num_iterations = 100;
-  /*_calib_cams_limit = 0;
+  /*options.max_num_iterations = 100;
+  _calib_cams_limit = 0;
   for(_calib_views_limit=0;_calib_views_limit<proxy["views"];_calib_views_limit=std::min(_calib_views_limit*2+1,proxy["views"]))
-    solve_pinhole(options, proxy, lines, img_size, extrinsics, extrinsics_rel, proj, strong_proj_constr_weight, non_center_rest_weigth);*/
-  //options.max_num_iterations = 5000;
+    solve_pinhole(options, proxy, lines, img_size, extrinsics, extrinsics_rel, proj, strong_proj_constr_weight, non_center_rest_weigth);
+  options.max_num_iterations = 5000;*/
+  
+  printf("num views: %d/%d\n", _calib_views_limit, proxy["views"]);
   
   for(_calib_cams_limit=0;_calib_cams_limit<proxy["cams"];_calib_cams_limit=std::min(_calib_cams_limit*4+1,proxy["cams"])) {
     solve_pinhole(options, proxy, lines, img_size, extrinsics, extrinsics_rel, proj, strong_proj_constr_weight, non_center_rest_weigth);
