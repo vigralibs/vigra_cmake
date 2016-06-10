84e1912ad39f135b188e0453d7b78953bb0d1fa2
Francesco Biscani <bluescarni@gmail.com>
2016-06-10 15:44:20 +0200

    more work.

--- ModularDeps.cmake
+++ ModularDeps.cmake
@@ -5,18 +5,33 @@ endif()
 find_package(Git REQUIRED)
 
 function(add_dependency)
+  # Parse the options.
   set(options)
-  set(oneValueArgs NAME REPO)
+  set(oneValueArgs NAME REPO BRANCH COMMIT)
   cmake_parse_arguments(AD "${options}" "${oneValueArgs}" "" ${ARGN})
-  set(DEP_NAME "${AD_NAME}")
-  set(DEP_REPO "${AD_REPO}")
+  # The dep name must always be there.
+  if(NOT AD_NAME)
+    message(FATAL_ERROR "At least the name of a dependency must be specified when using add_dependency().")
+  endif()
+  # Repo could come from cache variables, or from function option. Cached variable overrides the
+  # function argument.
+  if(DEPENDENCY_${AD_NAME}_REPO)
+    message(STATUS "Setting the repository for dependency '${AD_NAME}' to the value from cache '${DEPENDENCY_${AD_NAME}_REPO}'.")
+    set(AD_REPO "${DEPENDENCY_${AD_NAME}_REPO}")
+  endif()
+  if(NOT AD_REPO)
+    message(FATAL_ERROR "The repository of the dependency was not specified in the add_dependency() call, nor it was found in the cached variables.")
+  endif()
+  if(AD_BRANCH AND AD_COMMIT)
+    message(FATAL_ERROR "At most one of branch and commit must be specified when using add_dependency().")
+  endif()
 
   # Determine if the dependency has already been satisfied.
-  if(${DEP_NAME}_DEP_SATISFIED)
-    message(STATUS "The dependency '${DEP_NAME}' is already satisfied.")
+  if(DEPENDENCY_${AD_NAME}_SATISFIED)
+    message(STATUS "The dependency '${AD_NAME}' is already satisfied.")
     return()
   endif()
-  message(STATUS "Adding the dependency '${DEP_NAME}'.")
+  message(STATUS "Adding the dependency '${AD_NAME}'.")
 
   # Create the deps/ directory if it does not exist already.
   if(EXISTS "${PROJECT_SOURCE_DIR}/deps")
@@ -27,27 +42,37 @@ function(add_dependency)
   endif()
 
   # Try to check out the dependency.
-  if(EXISTS "${PROJECT_SOURCE_DIR}/deps/${DEP_NAME}")
-    message(STATUS "The '${DEP_NAME}' dir exists already.")
+  if(EXISTS "${PROJECT_SOURCE_DIR}/deps/${AD_NAME}")
+    message(STATUS "The '${AD_NAME}' dir exists already.")
   else()
-    message(STATUS "Cloning the '${DEP_NAME}' dependency.")
-    execute_process(COMMAND "${GIT_EXECUTABLE}" "clone" "${DEP_REPO}" "${DEP_NAME}"
+    message(STATUS "Cloning the '${AD_NAME}' dependency.")
+    execute_process(COMMAND "${GIT_EXECUTABLE}" "clone" "${AD_REPO}" "${AD_NAME}"
       WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/deps"
       RESULT_VARIABLE RES
       ERROR_VARIABLE OUT
       OUTPUT_VARIABLE OUT)
     if(RES)
-      message(FATAL_ERROR "The clone command for '${DEP_NAME}' failed. The output is:\n====\n${OUT}\n====")
+      message(FATAL_ERROR "The clone command for '${AD_NAME}' failed. The output is:\n====\n${OUT}\n====")
     endif()
-    message(STATUS "'${DEP_NAME}' was successfully cloned.")
+    message(STATUS "'${AD_NAME}' was successfully cloned. The git clone is located at '${PROJECT_SOURCE_DIR}/deps/${AD_NAME}'")
   endif()
 
   # Add the subdirectory of the dependency.
-  add_subdirectory("${PROJECT_SOURCE_DIR}/deps/${DEP_NAME}")
+  add_subdirectory("${PROJECT_SOURCE_DIR}/deps/${AD_NAME}")
 
-  # Mark the dependency as satisfied.
-  set(${DEP_NAME}_DEP_SATISFIED YES CACHE BOOL "${DEP_NAME} dependency.")
-  mark_as_advanced(${DEP_NAME}_DEP_SATISFIED)
+  # Store dependency info as cached variables.
+  set(DEPENDENCY_${AD_NAME}_SATISFIED YES CACHE INTERNAL "")
+  mark_as_advanced(DEPENDENCY_${AD_NAME}_SATISFIED)
+  set(DEPENDENCY_${AD_NAME}_REPO "${AD_REPO}" CACHE STRING "")
+  mark_as_advanced(DEPENDENCY_${AD_NAME}_REPO)
+  if(AD_BRANCH)
+    set(DEPENDENCY_${AD_NAME}_BRANCH "${AD_BRANCH}" CACHE STRING "")
+    mark_as_advanced(DEPENDENCY_${AD_NAME}_BRANCH)
+  endif()
+  if(AD_COMMIT)
+    set(DEPENDENCY_${AD_NAME}_COMMIT "${AD_COMMIT}" CACHE STRING "")
+    mark_as_advanced(DEPENDENCY_${AD_NAME}_COMMIT)
+  endif()
 endfunction()
 
 # Mark as included.
