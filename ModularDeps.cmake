312dfa7f4cadb78119ea4f6abbcd05874f243712
Ullrich Koethe <ullrich.koethe@iwr.uni-heidelberg.de>
2016-06-12 13:24:35 +0200

    add_dependency(): support 'git subtree'

--- ModularDeps.cmake
+++ ModularDeps.cmake
@@ -5,45 +5,47 @@ endif()
 find_package(Git REQUIRED)
 
 function(add_dependency AD_NAME)
-  # Parse the options.
-  set(options)
-  set(oneValueArgs REPO BRANCH COMMIT)
-  cmake_parse_arguments(AD "${options}" "${oneValueArgs}" "" ${ARGN})
-
-  # Repo could come from cache variables, or from function option. Cached variable overrides the
-  # function argument.
-  if(DEPENDENCY_${AD_NAME}_REPO)
-    message(STATUS "Setting the repository for dependency '${AD_NAME}' to the value from cache '${DEPENDENCY_${AD_NAME}_REPO}'.")
-    set(AD_REPO "${DEPENDENCY_${AD_NAME}_REPO}")
-  elseif(AD_REPO)
-    message(STATUS "Setting the repository for dependency '${AD_NAME}' to the value '${AD_REPO}' specified to the add_dependency() call.")
-  else()
-    message(FATAL_ERROR "The repository of the dependency was not specified in the add_dependency() call, nor it was found in the cached variables.")
-  endif()
-  # Check the branch and commit arguments.
-  if(AD_BRANCH AND AD_COMMIT)
-    message(FATAL_ERROR "At most one of branch and commit must be specified when using add_dependency().")
-  endif()
+  message(STATUS "Adding dependency '${AD_NAME}'.")
 
   # Determine if the dependency has already been satisfied.
   if(DEPENDENCY_${AD_NAME}_SATISFIED)
-    message(STATUS "The dependency '${AD_NAME}' is already satisfied.")
+    message(STATUS "... already satisfied.")
     return()
   endif()
-  message(STATUS "Adding the dependency '${AD_NAME}'.")
 
   # Create the deps/ directory if it does not exist already.
-  if(EXISTS "${PROJECT_SOURCE_DIR}/deps")
-    message(STATUS "The '${PROJECT_SOURCE_DIR}/deps' directory already exists.")
-  else()
-    message(STATUS "The '${PROJECT_SOURCE_DIR}/deps' directory does not exist, creating it.")
+  if(NOT EXISTS "${PROJECT_SOURCE_DIR}/deps")
+    message(STATUS "Directory '${PROJECT_SOURCE_DIR}/deps' does not exist, creating it.")
     file(MAKE_DIRECTORY "${PROJECT_SOURCE_DIR}/deps")
   endif()
 
-  # Try to check out the dependency.
+  # Check if the deps/${AD_NAME} already exists.
   if(EXISTS "${PROJECT_SOURCE_DIR}/deps/${AD_NAME}")
-    message(STATUS "The '${AD_NAME}' dir exists already.")
+    message(STATUS "Directory '${PROJECT_SOURCE_DIR}/deps/${AD_NAME}' exists. Assuming")
+    message(STATUS "dependency '${AD_NAME}' is satisfied via 'git subtree' or similar.")
   else()
+
+    # Parse the options.
+    set(options)
+    set(oneValueArgs REPO BRANCH COMMIT)
+    cmake_parse_arguments(AD "${options}" "${oneValueArgs}" "" ${ARGN})
+
+    # Repo could come from cache variables, or from function option. Cached variable overrides the
+    # function argument.
+    if(DEPENDENCY_${AD_NAME}_REPO)
+      message(STATUS "Setting the repository for dependency '${AD_NAME}' to the value from cache '${DEPENDENCY_${AD_NAME}_REPO}'.")
+      set(AD_REPO "${DEPENDENCY_${AD_NAME}_REPO}")
+    elseif(AD_REPO)
+      message(STATUS "Setting the repository for dependency '${AD_NAME}' to the value '${AD_REPO}' specified to the add_dependency() call.")
+    else()
+      message(FATAL_ERROR "The repository of the dependency was not specified in the add_dependency() call, nor it was found in the cached variables.")
+    endif()
+    # Check the branch and commit arguments.
+    if(AD_BRANCH AND AD_COMMIT)
+      message(FATAL_ERROR "At most one of branch and commit must be specified when using add_dependency().")
+    endif()
+
+    # Try to check out the dependency.
     message(STATUS "Cloning the '${AD_NAME}' dependency.")
     execute_process(COMMAND "${GIT_EXECUTABLE}" "clone" "${AD_REPO}" "${AD_NAME}"
       WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/deps"
@@ -56,14 +58,17 @@ function(add_dependency AD_NAME)
     message(STATUS "'${AD_NAME}' was successfully cloned. The git clone is located at '${PROJECT_SOURCE_DIR}/deps/${AD_NAME}'")
   endif()
 
+
   # Add the subdirectory of the dependency.
   add_subdirectory("${PROJECT_SOURCE_DIR}/deps/${AD_NAME}")
 
   # Store dependency info as cached variables.
   set(DEPENDENCY_${AD_NAME}_SATISFIED YES CACHE INTERNAL "")
   mark_as_advanced(DEPENDENCY_${AD_NAME}_SATISFIED)
-  set(DEPENDENCY_${AD_NAME}_REPO "${AD_REPO}" CACHE STRING "")
-  mark_as_advanced(DEPENDENCY_${AD_NAME}_REPO)
+  if(AD_REPO)
+    set(DEPENDENCY_${AD_NAME}_REPO "${AD_REPO}" CACHE STRING "")
+    mark_as_advanced(DEPENDENCY_${AD_NAME}_REPO)
+  endif()
   if(AD_BRANCH)
     set(DEPENDENCY_${AD_NAME}_BRANCH "${AD_BRANCH}" CACHE STRING "")
     mark_as_advanced(DEPENDENCY_${AD_NAME}_BRANCH)
