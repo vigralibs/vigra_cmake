140351d9f39993f469cf5b9d20cf25e7822e9d56
Francesco Biscani <bluescarni@gmail.com>
2016-09-26 16:08:11 +0200

    Further namespacing to prevent name clashes, plus fixes.

--- VigraAddDep.cmake
+++ VigraAddDep.cmake
@@ -12,12 +12,12 @@ else()
 endif()
 
 function(git_clone REPO NAME)
+  find_package(Git REQUIRED)
   message(STATUS "Git cloning repo ${REPO} into '${VAD_EXTERNAL_ROOT}/${NAME}'.")
   if(EXISTS "${VAD_EXTERNAL_ROOT}/${NAME}")
     message(STATUS "The path '${VAD_EXTERNAL_ROOT}/${NAME}' already exists, skipping clone.")
     return()
   endif()
-  find_package(Git REQUIRED)
   # Try to check out the dependency.
   execute_process(COMMAND "${GIT_EXECUTABLE}" "clone" "${REPO}" "${NAME}"
     WORKING_DIRECTORY "${VAD_EXTERNAL_ROOT}"
@@ -43,41 +43,41 @@ function(vad_reset_hooks)
       message(FATAL_ERROR "A git clone was requested for dependency ${NAME}, but no repository variable has been set.")
     endif()
     git_clone(${VAD_${NAME}_GIT_REPO} ${NAME})
-    add_subdirectory("${VAD_EXTERNAL_ROOT}/${NAME}" "${VAD_EXTERNAL_ROOT}/${NAME}/build_external_dep" EXCLUDE_FROM_ALL)
+    add_subdirectory("${VAD_EXTERNAL_ROOT}/${NAME}" "${VAD_EXTERNAL_ROOT}/${NAME}/build_external_dep")
   endfunction()
 endfunction()
 
-function(vigra_add_dep VAD_NAME)
+function(vigra_add_dep NAME)
   # Reset the hooks.
   vad_reset_hooks()
 
   # Parse the options.
   set(options SYSTEM LIVE)
-  set(oneValueArgs GIT_REPO_ARG GIT_BRANCH GIT_COMMIT)
-  cmake_parse_arguments(VAD "${options}" "${oneValueArgs}" "" ${ARGN})
+  set(oneValueArgs GIT_REPO GIT_BRANCH GIT_COMMIT)
+  cmake_parse_arguments(ARG_VAD_${NAME} "${options}" "${oneValueArgs}" "" ${ARGN})
 
   # Validate options.
   # SYSTEM and LIVE cannot be present at the same time.
-  if(VAD_SYSTEM AND VAD_LIVE)
-    message(FATAL_ERROR "Dependency '${VAD_NAME}' was added both as a SYSTEM dependency and as a LIVE dependency: only one choice can be active.")
+  if(ARG_VAD_${NAME}_SYSTEM AND ARG_VAD_${NAME}_LIVE)
+    message(FATAL_ERROR "Dependency '${NAME}' was added both as a SYSTEM dependency and as a LIVE dependency: only one choice can be active.")
   endif()
 
   # Check if the dependency is already satisfied.
-  if(VAD_DEP_${VAD_NAME}_SATISFIED)
-    message(STATUS "Dependency '${VAD_NAME}' already satisfied, skipping.")
+  if(VAD_DEP_${NAME}_SATISFIED)
+    message(STATUS "Dependency '${NAME}' already satisfied, skipping.")
     return()
   endif()
 
-  if(NOT VAD_SYSTEM AND NOT VAD_LIVE)
+  if(NOT ARG_VAD_${NAME}_SYSTEM AND NOT ARG_VAD_${NAME}_LIVE)
     # If neither SYSTEM nor LIVE were specified, assume SYSTEM.
-    message(STATUS "Dependency '${VAD_NAME}' not specified as a SYSTEM or LIVE dependency, assuming SYSTEM.")
-    set(VAD_SYSTEM YES)
-  elseif(VAD_SYSTEM)
+    message(STATUS "Dependency '${NAME}' not specified as a SYSTEM or LIVE dependency, assuming SYSTEM.")
+    set(ARG_VAD_${NAME}_SYSTEM YES)
+  elseif(ARG_VAD_${NAME}_SYSTEM)
     # SYSTEM dependency required.
-    message(STATUS "Dependency '${VAD_NAME}' specified as a SYSTEM dependency.")
+    message(STATUS "Dependency '${NAME}' specified as a SYSTEM dependency.")
   else()
     # LIVE dependency required.
-    message(STATUS "Dependency '${VAD_NAME}' specified as a LIVE dependency.")
+    message(STATUS "Dependency '${NAME}' specified as a LIVE dependency.")
   endif()
 
   # if(VAD_GIT_BRANCH AND VAD_GIT_COMMIT)
@@ -85,61 +85,66 @@ function(vigra_add_dep VAD_NAME)
   # endif()
 
   # First thing, we try to read the dep properties from the VAD file, if provided.
-  find_file(VAD_${VAD_NAME}_FILE VAD_${VAD_NAME}.cmake ${CMAKE_MODULE_PATH})
-  if(VAD_${VAD_NAME}_FILE)
-    message(STATUS "VAD file 'VAD_${VAD_NAME}.cmake' was found at '${VAD_${VAD_NAME}_FILE}'.")
-    include(${VAD_${VAD_NAME}_FILE})
+  find_file(VAD_${NAME}_FILE VAD_${NAME}.cmake ${CMAKE_MODULE_PATH})
+  if(VAD_${NAME}_FILE)
+    message(STATUS "VAD file 'VAD_${NAME}.cmake' was found at '${VAD_${NAME}_FILE}'.")
+    include(${VAD_${NAME}_FILE})
     if(GIT_REPO)
-      message(STATUS "Git repo for dependency ${VAD_NAME} read from VAD file: ${GIT_REPO}")
+      message(STATUS "VAD file for dependency ${NAME} specifies git repo: ${GIT_REPO}")
       set(VAD_${NAME}_GIT_REPO_FILE ${GIT_REPO})
       unset(GIT_REPO)
     endif()
+    # TODO: branch/commit.
     if(GIT_BRANCH)
-      message(STATUS "Git branch for dependency ${VAD_NAME} read from VAD file: ${GIT_BRANCH}")
+      message(STATUS "Git branch for dependency ${NAME} read from VAD file: ${GIT_BRANCH}")
       set(VAD_GIT_BRANCH_CUR ${GIT_BRANCH})
       unset(GIT_BRANCH)
     endif()
     if(GIT_COMMIT)
-      message(STATUS "Git commit for dependency ${VAD_NAME} read from VAD file: ${GIT_COMMIT}")
+      message(STATUS "Git commit for dependency ${NAME} read from VAD file: ${GIT_COMMIT}")
       set(VAD_GIT_COMMIT_CUR ${GIT_COMMIT})
       unset(GIT_COMMIT)
     endif()
-    unset(VAD_${VAD_NAME}_FILE CACHE)
   else()
-    message(STATUS "No VAD file 'VAD_${VAD_NAME}.cmake' was found.")
+    message(STATUS "No VAD file 'VAD_${NAME}.cmake' was found.")
   endif()
+  # Unset the variable from cache.
+  unset(VAD_${NAME}_FILE CACHE)
 
   # TODO: same for branch and commit.
-  if(VAD_${VAD_NAME}_GIT_REPO)
+  if(VAD_${NAME}_GIT_REPO)
     # Git repo was passed from command line or this is not the first run.
-    message(STATUS "Final git repo address for dependency ${VAD_NAME} is from cache: ${VAD_${VAD_NAME}_GIT_REPO}")
+    message(STATUS "Final git repo address for dependency ${NAME} is from cache: ${VAD_${NAME}_GIT_REPO}")
   else()
-    if(GIT_REPO_ARG)
+    if(ARG_VAD_${NAME}_GIT_REPO)
       # Git repo passed as function argument, overrides setting from file.
-      message(STATUS "Final git repo address for dependency ${VAD_NAME} is from function argument: ${GIT_REPO_ARG}")
-      set(VAD_${VAD_NAME}_GIT_REPO ${GIT_REPO_ARG} CACHE INTERNAL "")
+      message(STATUS "Final git repo address for dependency ${NAME} is from function argument: ${ARG_VAD_${NAME}_GIT_REPO}")
+      set(VAD_${NAME}_GIT_REPO ${ARG_VAD_${NAME}_GIT_REPO} CACHE INTERNAL "")
     elseif(VAD_${NAME}_GIT_REPO_FILE)
       # Git repository coming from the file.
-      message(STATUS "Final git repo address for dependency ${VAD_NAME} is from file: ${VAD_${NAME}_GIT_REPO_FILE}")
-      set(VAD_${VAD_NAME}_GIT_REPO ${VAD_${NAME}_GIT_REPO_FILE} CACHE INTERNAL "")
+      message(STATUS "Final git repo address for dependency ${NAME} is from file: ${VAD_${NAME}_GIT_REPO_FILE}")
+      set(VAD_${NAME}_GIT_REPO ${VAD_${NAME}_GIT_REPO_FILE} CACHE INTERNAL "")
     endif()
   endif()
 
-  if(VAD_SYSTEM)
-    vad_system(${VAD_NAME})
-  elseif(VAD_LIVE)
+  if(ARG_VAD_${NAME}_SYSTEM)
+    vad_system(${NAME})
+  elseif(ARG_VAD_${NAME}_LIVE)
     # Create the external deps directory if it does not exist already.
     if(NOT EXISTS "${VAD_EXTERNAL_ROOT}")
       message(STATUS "Directory '${VAD_EXTERNAL_ROOT}' does not exist, creating it.")
       file(MAKE_DIRECTORY "${VAD_EXTERNAL_ROOT}")
     endif()
-    vad_live(${VAD_NAME})
+    vad_live(${NAME})
   endif()
 
   # Mark the dep as satisfied.
-  message(STATUS "Marking dependency ${VAD_NAME} as satisfied.")
+  message(STATUS "Marking dependency ${NAME} as satisfied.")
   # NOTE: the flag must escape the function scope.
-  set(VAD_DEP_${VAD_NAME}_SATISFIED YES PARENT_SCOPE)
+  set(VAD_DEP_${NAME}_SATISFIED YES PARENT_SCOPE)
+
+  # Reset the hooks at the end as well for cleanup purposes.
+  vad_reset_hooks()
 endfunction()
 
 # Mark as included.
